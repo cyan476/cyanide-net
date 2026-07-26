184212c5a3bd25e06d28e5df518da4d7c8979740
cyan <191323534+cyan476@users.noreply.github.com>
2026-07-26 16:21:37 +0800

    e

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
-import netlify from '@astrojs/netlify';
+import node from '@astrojs/node';
 
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
+  adapter: node({ mode: 'standalone' }),
   vite: {
-    plugins: [
-      tailwindcss()
-    ]
+    plugins: [tailwindcss()]
   },
-  integrations: [
-    mdx()
-  ]
+  integrations: [mdx()]
 });
