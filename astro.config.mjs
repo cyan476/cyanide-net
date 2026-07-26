2e373d4502bd5718f5f74ceccd6c7eec75cfc9ef
cyan <191323534+cyan476@users.noreply.github.com>
2026-07-26 15:34:37 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
-import cloudflare from '@astrojs/cloudflare';
+
 
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
   vite: {
     plugins: [
       tailwindcss()
