a5dfa6b27944f274075c0489f572f6319dfb6b12
cyan <191323534+cyan476@users.noreply.github.com>
2026-07-26 15:01:31 +0800

    Update data.js to use client.js instead of the nonexistent client

--- src/lib/tina/data.ts
+++ src/lib/tina/data.ts
@@ -1,6 +1,6 @@
 // @ts-nocheck (generated types/client appear after your first tinacms dev run)
 import { requestWithMetadata } from '@tinacms/astro/data';
-import client from '../../../tina/__generated__/client';
+import client from '../../../tina/__generated__/client.js';
 
 export const getPost = (slug: string) =>
   requestWithMetadata(client.queries.post({ relativePath: slug + '.md' }), {
